03f04f65fb45551986076490808cb67bb4df866b
Shota <wvp360gqanukcqhg@gmail.com>
2023-12-31 17:33:41 +0900

    実装完了

--- api/tweet.go
+++ api/tweet.go
@@ -5,10 +5,50 @@ import (
 	"golang_twitter/db"
 	"golang_twitter/util"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
+
+func GetTweetsHandler(dbQueries *db.Queries) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ページネーションのパラメータを取得
+		page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+		if err != nil || page < 1 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "不正なページ番号"})
+			return
+		}
+
+		pageSize, err := strconv.Atoi(c.DefaultQuery("pageSize", "5"))
+		if err != nil || pageSize <= 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "不正なページサイズ"})
+			return
+		}
+
+		// セッションからログインユーザーのIDを取得
+		session := sessions.Default(c)
+		userID := session.Get("user_id")
+		if userID == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "ログインが必要です"})
+			return
+		}
+
+		// ログインユーザーのツイートをデータベースから取得
+		tweets, err := dbQueries.GetTweets(c, db.GetTweetsParams{
+			UserID: userID.(int32), // セッションから取得したユーザーIDをキャスト
+			Limit:  int32(pageSize),
+			Offset: int32((page - 1) * pageSize),
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "ツイートの取得に失敗しました"})
+			return
+		}
+
+		// レスポンスを返す
+		c.JSON(http.StatusOK, tweets)
+	}
+}
 
 func PostTweetHandler(dbQueries *db.Queries) gin.HandlerFunc {
 	return func(c *gin.Context) {
---
+++ db/sql/get_tweets.sql
@@ -0,0 +1,4 @@
+-- name: GetTweets :many
+SELECT * FROM tweets WHERE user_id = $1
+ORDER BY created_at DESC
+LIMIT $2 OFFSET $3;
